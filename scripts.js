8f39b1459c7a15a61caf6efa9eb682187565a650
Fmuller07 <ventaniamuller@gmail.com>
2024-12-04 09:02:52 -0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -19,10 +19,14 @@ function addLinha() {
     const celulaIdade = document.createElement("td");
     celulaIdade.textContent = "00";
 
+    const celulaTelefone = document.createElement("td");
+    celulaTelefone.textContent = "(00) 00000-0000";
+
     // Adicionando as células à nova linha
     novaLinha.appendChild(celulaId);
     novaLinha.appendChild(celulaNome);
     novaLinha.appendChild(celulaIdade);
+    novaLinha.appendChild(celulaTelefone);
 
     // Adicionando a nova linha ao corpo da tabela
     tbody.appendChild(novaLinha);
